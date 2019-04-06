091fa7dde34812c8aa4638bb9d70caa2553bd096
tifoaudy <165150200111168@mail.ub.ac.id>
2019-04-06 19:35:12 +0700

    create function for fetch and remove CoreData objects

--- iGoals/Controller/GoalVC.swift
+++ iGoals/Controller/GoalVC.swift
@@ -13,11 +13,19 @@ class GoalVC: UIViewController {
     
     //MARK: OUTLETS & PROPERTIES
     @IBOutlet weak var goalTableView: UITableView!
+    private var goals: [Goal] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.setupTableView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        self.refreshCoreDataObject()
+        self.goalTableView.reloadData()
     }
     
     @IBAction func addGoalButtonPressed(_ sender: Any) {
@@ -31,21 +39,80 @@ class GoalVC: UIViewController {
         goalTableView.isHidden = false
     }
     
+    private func refreshCoreDataObject() {
+        self.fetchData { (success) in
+            if success {
+                if goals.count < 1 {
+                    self.goalTableView.isHidden = true
+                } else {
+                    self.goalTableView.isHidden = false
+                }
+            }
+        }
+    }
+    
 }
 
 //MARK: EXTENSION TABLE VIEW DELEGATE & DATA SOURCE
 extension GoalVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return goals.count
     }
+    
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: GOAL_CELL, for: indexPath) as? GoalCell {
-            cell.setupCell(desc: "get hired as a traveloka engineer", type: .shortTerm, progress: 0)
+            let goal = goals[indexPath.row]
+            cell.setupCell(desc: goal.goalDescription!, type: GoalType(rawValue: goal.goalType!)!, progress: Int(goal.goalProgress))
             return cell
         } else {
             return GoalCell()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "DELETE") { (rowAction, indexPath) in
+            self.removeData(forIndexPath: indexPath)
+            self.refreshCoreDataObject()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        deleteAction.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        return [deleteAction]
+    }
+}
+
+extension GoalVC {
+    
+    func removeData(forIndexPath indexPath: IndexPath) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        managedContext.delete(goals[indexPath.row])
+        
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            debugPrint(error)
+        }
+    }
+    
+    func fetchData(completion: (_ status: Bool) -> ()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Goal")
+        
+        do {
+            goals = try managedContext.fetch(fetchRequest) as! [Goal]
+            completion(true)
+        } catch let error as NSError {
+            debugPrint(error)
+            completion(false)
         }
     }
 }
